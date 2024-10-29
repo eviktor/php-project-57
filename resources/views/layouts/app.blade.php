0e36caecc935a14504d632a8f272f2d87d4c0165
eviktor <vk7771199@gmail.com>
2024-10-29 08:58:01 +0700

    feat: Add welcome page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="lang="{{ str_replace('_', '-', app()->getLocale()) }}"">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-param" content="_token" />
+
+    <title>@yield('title', 'Менеджер задач')</title>
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <header class="fixed w-full">
+            <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
+                <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+                    <a href="/" class="flex items-center">
+                        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Менеджер задач</span>
+                    </a>
+                    <div class="flex items-center lg:order-2">
+                        <a href="/login"
+                            class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                            Вход
+                        </a>
+                        <a href="/register"
+                            class="px-4 py-2 ml-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                            Регистрация
+                        </a>
+                    </div>
+
+                    <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <a href="/tasks" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                    Задачи
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/task_statuses" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                    Статусы
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/labels" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                    Метки
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <section class="bg-white dark:bg-gray-900">
+            @yield('content')
+        </section>
+    </div>
+</body>
+
+</html>
