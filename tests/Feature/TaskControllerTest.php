6efbf0a5c056ef848290b9eabf4f4a48242c77c9
eviktor <vk7771199@gmail.com>
2024-11-02 01:08:35 +0700

    test: Add tests for TaskController

---
+++ tests/Feature/TaskControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+use App\Models\Task;
+
+class TaskControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        Task::factory()->count(2)->make();
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('tasks.index'));
+        $response->assertOk();
+    }
+
+    public function testCreate()
+    {
+        $response = $this->get(route('tasks.create'));
+        $response->assertOk();
+    }
+
+    public function testEdit()
+    {
+        $task = Task::factory()->create();
+        $response = $this->get(route('tasks.edit', [$task]));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = Task::factory()->make()->only('name', 'status_id');
+        $response = $this->post(route('tasks.store'), $data);
+        $response->assertRedirect(route('tasks.index'));
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    public function testUpdate()
+    {
+        $task = Task::factory()->create();
+        $data = Task::factory()->make()->only('name', 'status_id');
+
+        $response = $this->patch(route('tasks.update', $task), $data);
+        $response->assertRedirect(route('tasks.index'));
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    public function testDestroy()
+    {
+        $task = Task::factory()->create();
+        $response = $this->delete(route('tasks.destroy', [$task]));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('tasks.index'));
+
+        $this->assertDatabaseMissing('tasks', $task->only('id'));
+    }
+}
