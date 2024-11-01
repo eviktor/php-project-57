e68f2c3d58097190b992fdbff2e7baf8d68d7bc7
eviktor <vk7771199@gmail.com>
2024-11-01 17:43:29 +0700

    feat: Add relations to models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,19 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'status_id', 'assigned_to_id'];
+
+    public function status()
+    {
+        return $this->belongsTo(TaskStatus::class, 'status_id');
+    }
+
+    public function assigned_to()
+    {
+        return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+
+    public function created_by()
+    {
+        return $this->belongsTo(User::class, 'created_by_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, "created_by_id");
+    }
+
+    public function assigned_tasks()
+    {
+        return $this->hasMany(Task::class, 'assigned_to_id');
+    }
 }
